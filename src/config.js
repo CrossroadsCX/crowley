7bcec95d9fea7f188567997759bccd478b0799cf
Chris Birk <cmbirk@gmail.com>
2020-08-06 22:49:28 -0500

    set up config correctly

--- src/config.js
+++ src/config.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  CLIENT_SIGNING_SECRET: process.env.CLIENT_SIGNING_SECRET,
-  PORT: process.env.PORT || 3000,
-  VERIFICATION_TOKEN: process.env.VERIFICATION_TOKEN,
-  WEBHOOK_URI: process.env.WEBHOOK_URI,
+const {
+  BOT_TOKEN,
+  CLIENT_SIGNING_SECRET,
+  PORT = 3000,
+  VERIFICATION_TOKEN,
+  WEBHOOK_URI,
+} = process.env
+
+export {
+  BOT_TOKEN,
+  CLIENT_SIGNING_SECRET,
+  PORT,
+  VERIFICATION_TOKEN,
+  WEBHOOK_URI,
 }
